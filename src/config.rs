8a0916612de5c79a1db8d1ed9b3c7a2c7388cd43
norberts.kakste <nk15012@lu.lv>
2017-04-25 18:08:37 +0300

    Initial commit --------------------------- * Added basic setting enums.

---
+++ src/config.rs
@@ -0,0 +1,32 @@
+/// File Reading settings (Memory)
+///
+/// Full = Reads the whole file in memory
+/// Partial(u32) = Reads the specified amount of "protobufs" in memory
+/// Auto = Minimal memory usage (1 "protobuf" spec per allocation)
+enum FileReadMemorySettings {
+    Full, // Reads the whole file in memory
+    Partial(u32), // Reads the specified amount of "protobufs" in memory
+    Auto, // Minimal memory usage (1 "protobuf" spec per allocation
+}
+
+/// File Reading settings (Concurrency)
+///
+/// Full = Spawns n amount of workers, where n = CPU threads
+/// Partial(u32) = Spawns the specified amount of workers
+/// Seq = Single thread
+enum FileReadConcurrencySettings {
+    Full, // Spawns n amount of workers, where n = CPU threads
+    Partial(u32), // Spawns the specified amount of workers
+    Seq, // Single thread
+}
+
+/// Replay parsing settings
+///
+/// All = Reads & parses everything
+/// Manual = Reads & parses specified events
+/// Callbacks = Reads & parses on callback registration
+enum ParsingPropertiesMode {
+    All, // Reads & parses everything
+    Manual, // Reads & parses specified events
+    Callbacks, // Reads & parses on callback registration
+}
